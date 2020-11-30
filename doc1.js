da599fdbc620b00f3efc56f7bb09c34801401e0f
allan sousa <allan@pop-os.localdomain>
2020-11-30 17:53:54 -0300

    criando html

--- doc1.js
+++ doc1.js
@@ -1,7 +1,18 @@
+<script>
+// Cosole Log
 console.log("hello Wold") //console mostra na tela uma mensage de Olá mundo
 
+// Comentários
 //Comentário em uma linha 
 
 /*
 Bloco de comentário
 */
+
+// Tipos de variáveis 
+
+/* Data types
+undefinend, null, boolean, string, symbol, number, and obejct
+*/
+
+</script>
